188095b23aa3a122485368062cbd574c6e36d8cd
Md Rokon Uzzaman <89872209+mrokonuzzaman040@users.noreply.github.com>
2024-07-08 12:37:30 +0600

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -41,7 +41,7 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: false, // true for 465, false for other ports
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
