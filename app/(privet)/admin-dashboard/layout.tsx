eaa4a57ed21adc0c3be0f70760306553648beea7
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-07-26 14:53:30 +0600

    refactor: Remove react-paypal-button-v2 dependency and update package-lock.json and package.json

--- app/(privet)/admin-dashboard/layout.tsx
+++ app/(privet)/admin-dashboard/layout.tsx
@@ -95,7 +95,7 @@ export default function Layout({
                     <div className="flex justify-between items-center py-2">
                         <ol className="ms-3 flex items-center whitespace-nowrap">
                             <li className="flex items-center text-sm text-gray-800 dark:text-neutral-400">
-                                User Panel
+                                Admin Panel
                                 <svg className="flex-shrink-0 mx-3 overflow-visible size-2.5 text-gray-400 dark:text-neutral-500" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M5 1L10.6869 7.16086C10.8637 7.35239 10.8637 7.64761 10.6869 7.83914L5 14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
                                 </svg>
